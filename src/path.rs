cd5139b4819ed5a5fdd2dcb02031a11401cfc485
emil-aagaard <emil@eacit.dk>
2026-03-12 09:19:51 +0100

    Fixed borrow bug

--- src/path.rs
+++ src/path.rs
@@ -8,15 +8,15 @@ pub fn get_output_path(
     output_directory: &Option<String>,
     output_file_name: &String,
 ) -> Result<PathBuf, Error> {
-    let output_directory = if let Some(output_directory) = output_directory {
-        output_directory
+    let output_directory = if let Some(output_directory_value) = output_directory {
+        output_directory_value.clone()
     } else {
         let home_env_var_name = if cfg!(target_os = "windows") {
             "USERPROFILE"
         } else {
             "HOME"
         };
-        &var(home_env_var_name)
+        var(home_env_var_name)
             .map_err(|_| Error::MissingHomeEnvVar(home_env_var_name.to_string()))?
     };
     Ok(PathBuf::from(output_directory).join(output_file_name))
